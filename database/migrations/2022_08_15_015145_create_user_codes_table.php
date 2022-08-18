67560e896a2109f41866763c38c3d416d03c95e7
mac <timson.babajide@gmail.com>
2022-08-18 18:52:21 +0100

    update user code migration

--- database/migrations/2022_08_15_015145_create_user_codes_table.php
+++ database/migrations/2022_08_15_015145_create_user_codes_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->foreignId('user_id')
                   ->constrained()->onDelete('cascade');
             $table->string('code');
-            $table->timestamp('confirmed_at');
+            $table->timestamp('confirmed_at')->nullable();
             $table->timestamps();
         });
     }
