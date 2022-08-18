54b0c841aa8ba138ee9a85e83c360930b0f991a4
mac <timson.babajide@gmail.com>
2022-08-18 21:04:06 +0100

    fixed error

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,7 +16,7 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 window.axios.defaults.withCredentials = true;
 
-window.axios.defaults.baseURL = process.env.APP_URL;
+window.axios.defaults.baseURL = "https://blackyrestaurant.herokuapp.com";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
