7fc2838b87a8bbcc3aa9a930ba09eaf2b83082f2
mac <timson.babajide@gmail.com>
2022-08-18 21:16:17 +0100

    fixed error

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,8 +16,6 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 window.axios.defaults.withCredentials = true;
 
-window.axios.defaults.baseURL = "https://blackyrestaurant.herokuapp.com";
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
