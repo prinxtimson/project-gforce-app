108584fdf71573e95ce9251fa7e9e6978d93f978
mac <timson.babajide@gmail.com>
2022-09-08 17:50:42 +0100

    update migration

--- database/migrations/2022_09_06_184509_add_column_to_products_table.php
+++ database/migrations/2022_09_06_184509_add_column_to_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('featured_image');
+            $table->string('featured_image')->nullable();
         });
     }
 
--- database/migrations/2022_09_08_105144_add_column_to_payments_table.php
+++ database/migrations/2022_09_08_105144_add_column_to_payments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->string('name');
+            $table->string('name')->nullable();
         });
     }
 
